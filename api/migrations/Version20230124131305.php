f8df5ffba0d0ab1c528f5934aff1785a900b9424
corneliusthefirst <ndeffo.jugal98@gmail.com>
2023-01-25 22:24:49 +0100

    apis routes running plans added

--- api/migrations/Version20230124131305.php
+++ api/migrations/Version20230124131305.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230123160526 extends AbstractMigration
+final class Version20230124131305 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20230123160526 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE company ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE invoice ADD updated_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE company DROP created_at, DROP updated_at');
+        $this->addSql('ALTER TABLE invoice DROP updated_at');
     }
 }
