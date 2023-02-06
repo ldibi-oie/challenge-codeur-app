17c22c0132d5c1fe75af0739540e3fef48c17a2a
bouba-coder <boubacaryatera1@gmail.com>
2023-02-06 22:25:03 +0100

    adding fixtures

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -5,17 +5,23 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use App\Entity\Company;
 <<<<<<< HEAD
+<<<<<<< HEAD
 use App\Entity\Freelance;
 use App\Entity\MediaObject;
 use App\Entity\Offer;
 use App\Entity\Subscription;
 use App\Entity\Freelance;
 =======
+=======
+>>>>>>> ce40ef7e (addnig fixtures)
 use App\Entity\Offer;
 use App\Entity\Subcription;
 use App\Entity\Freelance;
 use App\Entity\Keyword;
+<<<<<<< HEAD
 >>>>>>> 49171233 (adding new fixtures)
+=======
+>>>>>>> ce40ef7e (addnig fixtures)
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
@@ -126,6 +132,52 @@ class AppFixtures extends Fixture
             $freelance->addKeyword($keyword);
 
             $subcription = new Subscription();
+            $subcription->setSubcriptiontype("annuel");
+            $company->setSubscription($subcription);
+
+            $manager->persist($keyword);
+            $manager->persist($offer);
+            $manager->persist($company);
+            $manager->persist($subcription);
+            $manager->persist($freelance);
+        }
+        $manager->flush();
+        //fixtures 
+        for($i = 0; $i < 10; $i++) 
+        {
+            $company = new Company();
+            $company->setName("test".$i);
+            $company->setSiretnumber(123456789);
+            $company->setRole(["ROLE_COMPANY"]);
+            $company->setEmail("boubacar1".$i."@live.fr");
+            $company->setAddress("2 rue de symfony, 75018 php");
+            
+            $freelance = new Freelance();
+            $freelance->setName("test".$i);
+            $freelance->setSurname("last".$i);
+            $freelance->setSiretnumber(123456789);
+            $freelance->setRole(["ROLE_FREELANCE"]);
+            $freelance->setBirthday("5/2/2022");
+            $freelance->setEmail("freelane".$i."@gmail.com");
+            $freelance->setIsverified(false);
+            $freelance->setPassword("234678G");
+            $freelance->setFreelancetypes(["dev","graphisme","video"]);
+            $freelance->setCvlink("www.google.fr");
+
+            $offer = new Offer();
+            $offer->setCompany($company);
+            $offer->setTitle("offre".$i);
+            $offer->setDescription("description de l'offre".$i);
+            $freelance->addOffer($offer);
+
+            $keyword = new Keyword();
+            $keyword->setName("dev".$i);
+            $keyword->setOffer($offer);
+            $company->setOfferlist([$offer]);
+            $offer->addKeyword($keyword);
+            $freelance->addKeyword($keyword);
+
+            $subcription = new Subcription();
             $subcription->setSubcriptiontype("annuel");
             $company->setSubscription($subcription);
 
