20caef3b8c9cf17ee0e62611303b36d23e475aad
corneliusthefirst <ndeffo.jugal98@gmail.com>
2023-02-10 19:42:17 +0100

    working on offer creation from scrapper

--- frontend/src/stores/api.js
+++ frontend/src/stores/api.js
@@ -1,22 +1,22 @@
-import {defineStore} from 'pinia'
-import http from './../common/http-common'
+import { defineStore } from "pinia";
+import http from "./../common/http-common";
 
 export const useUserStore = defineStore("user", {
-    state: () => ({users: []}),
-    getters: {
-        get(state) {
-            return state.users
-        }
+  state: () => ({ users: [] }),
+  getters: {
+    get(state) {
+      return state.users;
     },
-    actions: {
-        async fetchUsers(url) {
-            try {
-                const response = await http.get(url)
-                this.users = response.data
-            } catch (error) {
-                alert(error)
-                console.log(error)
-            }
-        }
-    }
-})
+  },
+  actions: {
+    async fetchUsers(url) {
+      try {
+        const response = await http.get(url);
+        this.users = response.data;
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    },
+  },
+});
---
+++ frontend/src/views/OfferScrapper/OfferScrapperView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>text page</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "OfferScrapperView",
+  data() {
+    return {
+      query: "php",
+      fromdays: 1,
+      level: "senior_level",
+    };
+  },
+  mounted: function () {},
+};
+</script>
